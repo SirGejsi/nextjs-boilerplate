354cafec3e5ed0c6ac79a390ef6968b150c5b7f1
SirGejsi <gejsihalili@hotmail.com>
2024-10-01 21:52:50 +0100

    Create page.tsx

---
+++ app/questions/question5/page.tsx
@@ -0,0 +1,7 @@
+export default function Question5() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to Question 5</h1>
+    </div>
+  );
+}
