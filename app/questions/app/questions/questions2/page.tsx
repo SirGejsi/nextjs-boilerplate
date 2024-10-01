5ed327361a6602a7893e976393c9eae711a0e56e
SirGejsi <gejsihalili@hotmail.com>
2024-10-01 21:42:41 +0100

    Delete app/questions/app/questions/questions2 directory

--- app/questions/app/questions/questions2/page.tsx
+++
@@ -1,8 +0,0 @@
-// For Question 3
-export default function Question3() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Welcome to Question 2</h1>
-    </div>
-  );
-}
