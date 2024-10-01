0de70bbcbdd823c0c3efdc073c01183c01ac0ede
SirGejsi <gejsihalili@hotmail.com>
2024-10-01 21:15:48 +0100

    Update page.tsx

--- app/question2/page.tsx
+++ app/question2/page.tsx
@@ -1,1 +1,7 @@
-
+export default function Question2() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to Question 2</h1>
+    </div>
+  );
+}
