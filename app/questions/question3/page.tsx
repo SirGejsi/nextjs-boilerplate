2f87cdb8e072e653aa825fcdeb2228f48ab83e67
SirGejsi <gejsihalili@hotmail.com>
2024-10-03 23:50:41 +0100

    Update page.tsx

--- app/questions/question3/page.tsx
+++ app/questions/question3/page.tsx
@@ -1,7 +1,20 @@
-export default function Question3() {
+import Image from "next/image";
+
+export default function About() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Welcome to Question 3</h1>
-    </div>
-  );
-}
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="flex justify-between items-center w-full">
+        <div className="text-xl font-bold">Digital Success Take Home Assignment</div>
+        <div className="flex items-center">
+          {/* "By" text */}
+          <span className="mr-2">By</span>
+          {/* Vercel logo */}
+          <Image
+            className="invert"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Vercel_logo_black.svg"
+            alt="Vercel Logo"
+            width={100}
+            height={24}
+          />
+        </div>
+      </header>
