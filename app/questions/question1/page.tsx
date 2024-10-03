89e30fd64b5d040892f759d83905024aba69f0f2
SirGejsi <gejsihalili@hotmail.com>
2024-10-03 21:10:47 +0100

    Update page.tsx

--- app/questions/question1/page.tsx
+++ app/questions/question1/page.tsx
@@ -1,9 +1,9 @@
 export default function Question1() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="text-2xl font-bold">Welcome to Question 1</h1>
+      <h1 className="text-2xl font-bold">Question 1. Please compare the similarities and differences between Next.js and React.</h1>
       <p className="text-lg mt-4 max-w-2xl">
-        1. The main difference between React and Next.js is that React is just a UI library for building interfaces, so you get a lot of flexibility in how you structure your app, but you’ll need to add things like routing or server-side rendering yourself. On the other hand, Next.js is more of a full framework built on React, and it comes with everything you need, like file-based routing, SSR, and API routes, all built-in. So, if you’re building something more complex, like a site that needs good SEO or has dynamic content, Next.js takes care of a lot of the heavy lifting for you.
+        The main difference between React and Next.js is that React is just a UI library for building interfaces, so you get a lot of flexibility in how you structure your app, but you’ll need to add things like routing or server-side rendering yourself. On the other hand, Next.js is more of a full framework built on React, and it comes with everything you need, like file-based routing, SSR, and API routes, all built-in. So, if you’re building something more complex, like a site that needs good SEO or has dynamic content, Next.js takes care of a lot of the heavy lifting for you.
         <br /><br />
         Another key thing is how they handle rendering. With React, it’s mostly client-side rendering, which means your content is loaded in the browser and requires some extra work to get things like SEO and performance right. But with Next.js, you can use server-side rendering, static site generation, or even a mix, so it’s super optimized for faster loading and better SEO.
       </p>
