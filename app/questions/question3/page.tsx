aadb77230129350d5f4f8ba40f5ca369a0494ddd
SirGejsi <gejsihalili@hotmail.com>
2024-10-04 00:28:50 +0100

    Update page.tsx

--- app/questions/question3/page.tsx
+++ app/questions/question3/page.tsx
@@ -1,3 +1,34 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="flex justify-between items-center w-full">
+        <div className="text-xl font-bold">Digital Success Take Home Assignment</div>
+        <div className="flex items-center">
+          {/* "By" text */}
+          <span className="mr-2">By</span>
+          {/* Vercel logo */}
+          <Image
+            className="invert"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Vercel_logo_black.svg"
+            alt="Vercel Logo"
+            width={100}
+            height={24}
+          />
+        </div>
+      </header>
+
+      <main className="flex flex-col items-center">
+        <h2 className="text-2xl font-bold mt-12">Question 1</h2>
+        <p className="text-lg mt-2 italic">
+          Please compare the similarities and differences between Next.js and React.
+        </p>
+      </main>
+    </div>
+  );
+}
+
 export default function Email() {
   return (
     <div className="min-h-screen flex items-center justify-center p-8">
