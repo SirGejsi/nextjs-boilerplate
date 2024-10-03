ff57d922af45a2974d274c8da16a6a03cf28f5ae
SirGejsi <gejsihalili@hotmail.com>
2024-10-03 22:15:34 +0100

    Update page.tsx

--- app/questions/question1/page.tsx
+++ app/questions/question1/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* Add a div for the header section */}
       <header className="flex justify-between items-center w-full">
         <div className="text-xl font-bold">Digital Success Take Home Assignment</div>
         <div className="flex items-center">
