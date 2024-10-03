c011368d199dbce46946a8057a98162f384c74cb
SirGejsi <gejsihalili@hotmail.com>
2024-10-03 21:44:48 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Add a div for the header section */}
       <header className="flex justify-between items-center w-full">
-        <div className="text-xl font-bold">Digital Success Take Home Assignment Example</div>
+        <div className="text-xl font-bold">Digital Success Take Home Assignment</div>
         <div className="flex items-center">
           {/* Vercel logo */}
           <Image
