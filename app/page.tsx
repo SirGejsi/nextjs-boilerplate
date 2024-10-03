96528fa23527fcc04fc51ff60371fb82c6333196
SirGejsi <gejsihalili@hotmail.com>
2024-10-03 22:16:03 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* Add a div for the header section */}
       <header className="flex justify-between items-center w-full">
         <div className="text-xl font-bold">Digital Success Take Home Assignment</div>
         <div className="flex items-center">
@@ -15,8 +14,8 @@ export default function Home() {
             className="invert"
             src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Vercel_logo_black.svg"
             alt="Vercel Logo"
-            width={100} // Adjust the width as needed
-            height={24} // Adjust the height as needed
+            width={100}
+            height={24}
           />
         </div>
       </header>
