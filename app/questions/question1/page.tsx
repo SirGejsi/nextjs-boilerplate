5979b2b8ea41004cb47d9eda90fbc4d8d4c058d3
SirGejsi <gejsihalili@hotmail.com>
2024-10-04 01:50:57 +0100

    Update page.tsx

--- app/questions/question1/page.tsx
+++ app/questions/question1/page.tsx
@@ -20,17 +20,14 @@ export default function About() {
       </header>
 
       <main className="flex flex-col items-center">
-        <h2 className="text-2xl font-bold mt-12">Question 4</h2>
+        <h2 className="text-2xl font-bold mt-12">Question 1</h2>
         <p className="text-lg mt-2 italic">
-          A customer who has been sold Vercel through a reseller has reached out to the team to ask about the product. Can you give a brief 2-5 sentence introduction to Vercel to showcase what Vercel does?
+          Please compare the similarities and differences between Next.js and React
         </p>
 
         {/* Email Component */}
         <div className="bg-white border border-gray-300 rounded-lg shadow-md p-6 max-w-2xl mt-4">
           <p className="text-sm text-black">
-            Vercel is a powerful platform designed to help developers deploy, manage, and optimize frontend web applications effortlessly. It provides a frontend cloud that offers features like instant deploys, serverless functions, and edge network optimizations. This makes it ideal for projects that need high-performance websites and web apps with minimal setup. Plus, Vercel is the creator of Next.js, a popular React framework, which further enhances its ease of use for modern web development.
-            <br />
-            <br />
             The main difference between React and Next.js is that React is just a UI library for building interfaces, so you get a lot of flexibility in how you structure your app, but you’ll need to add things like routing or server-side rendering yourself. On the other hand, Next.js is more of a full framework built on React, and it comes with everything you need, like file-based routing, SSR, and API routes, all built-in. So, if you’re building something more complex, like a site that needs good SEO or has dynamic content, Next.js takes care of a lot of the heavy lifting for you.
             <br />
             <br />
